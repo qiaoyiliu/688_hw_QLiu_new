6059b3751b1e91cd201796c5356d96c5e693fafe
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-27 15:47:29 +0000

    3ehww3gw3

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ hw2 = st.Page("streamlit_app_hw2.py", title="HW 2", icon=":material/add_circle:"
 hw3 = st.Page("streamlit_app_hw3.py", title="HW 3", icon=":material/add_circle:")
 hw4 = st.Page("streamlit_app_hw4.py", title="HW 4", icon=":material/add_circle:")
 hw5 = st.Page("streamlit_app_hw5.py", title="HW 5", icon=":material/add_circle:")
+test = st.Page("streamlit_app_test.py", title="Test", icon=":material/add_circle:")
 
-pg = st.navigation([hw1, hw2, hw3, hw4, hw5])
+pg = st.navigation([hw1, hw2, hw3, hw4, hw5, test])
 st.set_page_config(page_title="688HW", page_icon=":material/edit:")
 pg.run()
